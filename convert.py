718126a18cb0d783a84329dc2a31dbc9a3a9a04b
kabrooksRUSH <kylebrooks0125@gmail.com>
2023-09-10 20:40:55 -0400

    comment for clarity

--- convert.py
+++ convert.py
@@ -1,10 +1,12 @@
 import cv2 as cv
 import os
+
+"""
+Simple script to convert the output images from .pgm to .png
+"""
+
 path = "E:/Professional/CovarianceTracking/Output/"
 
 for fname in os.listdir(path):
     img = cv.imread("./Output/" + fname)
-    # cv.imshow("name", img)
-    # print("".join(fname.split('.')[0:-1]) + ".png")
-
     cv.imwrite("".join(fname.split('.')[0:-1]) + ".png", img)
